9048f6152dd67d496db26d54c6351c5009faeb32
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-15 12:09:30 +0530

    Create variable.tf

---
+++ module/subnet/variable.tf
@@ -0,0 +1,9 @@
+variable "public_subnet_cidr_1" {
+  type = string
+  default = "192.168.1.0/24"
+}
+variable "public_subnet_tag_1" {
+    type = string
+    default = "tcw_public_subnet_az_1a"
+}
+
