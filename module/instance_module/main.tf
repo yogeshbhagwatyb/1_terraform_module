9b99f1afce65f21aa43446529dab77e1a8093e96
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-15 11:48:43 +0530

    Update main.tf

--- module/instance_module/main.tf
+++ module/instance_module/main.tf
@@ -1,1 +1,18 @@
+variable "cidr" {
+  type = string
+  default = "192.168.0.0/16"
+}
 
+variable "enable_dns_hostnames" {
+  type = bool
+  default = null
+}
+variable "enable_dns_support" {
+  type = bool
+  default = null
+}
+
+variable "vpc_name" {
+  type = string
+  default = "tcw_vpc"
+}
